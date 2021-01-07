ed51f0ae69fecfd01ee343d9c5ec4a3324158ba5
vyu_an <vyu.an.an@yandex.ru>
2021-01-07 15:05:40 +0300

    Password reset

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,6 @@
 from django.urls import path, include
 from django.contrib.auth import views as auth_views
 
-
 # app_name = 'account'
 
 from account import views
@@ -25,5 +24,13 @@ urlpatterns = [
     # path('password_change/done/', auth_views.PasswordChangeDoneView.as_view(),
     #      name='password_change_done'),
 
-
-    ]
+    # Обработчики восстановления пароля.
+    # path('password_reset/', auth_views.PasswordResetView.as_view(),
+    #      name='password_reset'),
+    # path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(),
+    #      name='password_reset_done'),
+    # path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(),
+    #      name='password_reset_confirm'),
+    # path('reset/done/', auth_views.PasswordResetCompleteView.as_view(),
+    #      name='password_reset_complete'),
+]
